ae2fcb700f1949bd570bc6816d92479fa1ae631e
SHINMH <sky95012@gamil.com>
2020-06-01 17:34:47 +0900

    script.js start, close button revise

--- testapp/models.py
+++ testapp/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Post(models.Model):
-	title = models.CharField(max_length=100)
-	photo = models.ImageField(blank=True)
-
-
-class Post(models.Model):
-    profile_pic = models.ImageField(upload_to="testapp/profile_pic")
-    # 저장경로 : MEDIA_ROOT/blog/profile_pic/xxxx.jpg 경로에 저장
-    # DB필드 : 'MEDIA_URL/blog/profile_pic/xxxx.jpg' 문자열 저장
-    photo = models.ImageField(blank=True, upload_to="testapp/%Y/%m/%d")
-# 저장경로 : MEDIA_ROOT/blog/2017/05/10/xxxx.jpg 경로에 저장
-# DB필드 : 'MEDIA_URL/blog/2017/05/10/xxxx.jpg' 문자열 저장
--- testapp/static/js/script.js
+++ testapp/static/js/script.js
@@ -1,42 +1,28 @@
-
 //webcam으로 부터 데이터를 받아와 video로 보여줌
 var video = document.querySelector(".videoElement");
+
 if (navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then(function (stream) {
-            video.srcObject = stream;
-        })
-        .catch(function (err0r) {
-            console.log(err0r)
-            console.log("Something went wrong!");
-        });
+    navigator.mediaDevices.getUserMedia(vgaConstraints = {
+        video: {width: {min: 1280}, height: {min: 720}}
+    }).then(function (stream) {
+        video.srcObject = stream;
+    }).catch(function (err0r) {
+        console.log(err0r)
+        console.log("Something went wrong!");
+    });
 }
 
 var snapshotCanvas = document.getElementById('snapshot');
-var captureButton = document.getElementById('video_start');
-
-captureButton.addEventListener('click', function () {
-
-});
-
-var time = 1;
-autoCapture = setInterval(function () {
-    captureButton.click()
-
-    console.log(time)
-
-    if (time == 50) clearInterval(autoCapture)
-    time++;
-    //document.test3.submit()
-}, 100);
 
 function sendImage() {
-    var context = snapshotCanvas.getContext('2d');
-    // Draw the video frame to the canvas.
-    context.drawImage(video, 0, 0, snapshotCanvas.width,
-        snapshotCanvas.height);
+    var canvas = snapshotCanvas.getContext('2d');
+    // Draw the video frame to the canvas
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    canvas.drawImage(video, 0, 0);
     //이미지 전송
-    var dataUrl = snapshotCanvas.toDataURL("image/jpg", 1.0)
+    var dataUrl = snapshotCanvas.toDataURL("image/png")
     var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
     $.ajax({
         type: 'POST',
@@ -47,13 +33,23 @@ function sendImage() {
 
 var video_start = document.getElementById("video_start")
 var video_close = document.getElementById("video_close")
+var time;
 
 video_start.addEventListener('click', function () {
-    console.log('start')
-    sendImage()
+    video_start.disabled = true
+    time = 0;
+    autoCapture = setInterval(function () {
+        sendImage()
+        console.log(time)
+        time++;
+        if (time >= 50) {
+            video_close.disabled = false
+        }
+    }, 100);
 })
 
 video_close.addEventListener('click', function () {
-    console.log('close')
-
+    video_start.disabled = false
+    video_close.disabled = true
+    clearInterval(autoCapture)
 })
