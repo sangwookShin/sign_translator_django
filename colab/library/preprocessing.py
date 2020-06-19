01d427e3d6bb1497cb586b6b4a3c7fb98c8edd00
brothersoo <bigbechee@gmail.com>
2020-06-19 20:20:37 +0900

    Update preprocessing.py
    add minimize_face

--- colab/library/preprocessing.py
+++ colab/library/preprocessing.py
@@ -1,6 +1,18 @@
 import numpy as np
 import pandas as pd
 
+#pose : 0-23
+#face : 24-163
+    #lower-face-line : 24-57
+    #eye-brow : 58-77
+    #nose : 78-95
+    #eye : 96-119
+    #outer-mouth :  120-143
+    #inner-mouth : 144-159
+    #pupil : 160-163
+#left hand : 164-205
+#right hand : 205-247
+
 def delete_face(X_):
     #X_ must be pyhton list
     array = []
@@ -11,6 +23,27 @@ def delete_face(X_):
             temp_frame = X_[i][:24] #body
         else:
             temp_frame += X_[i][:24]
+        temp_frame += X_[i][164:248] #left hand and right hand
+        array.append(temp_frame)
+
+    X_ = pd.DataFrame(array)
+        
+    #returns pandas dataframe
+    return X_
+    
+def minimize_face(X_):
+        #X_ must be pyhton list
+        #include eye and outer-mouth
+    array = []
+
+    for i in range(len(X_)):
+        temp_frame=[]
+        if i == 0:
+            temp_frame = X_[i][:24] #body
+        else:
+            temp_frame += X_[i][:24] #body
+        temp_frame += X_[i][96:119] #eye
+        temp_frame += X_[i][120:143] #outer-mouth
         temp_frame += X_[i][164:248] #left hand and right hand
         array.append(temp_frame)
 
